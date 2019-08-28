1eadd0c25d579e09468bde97db9dacc9e1b5b8f4
Sarah Arnold <digipet007@gmail.com>
2019-08-27 18:15:12 -0700

    initial html, and Bootstrap completed

---
+++ javascript/java.js
@@ -0,0 +1,14 @@
+//SETUP VARIABLES
+//===============================================
+var authKey = "psE8JudDOYAJlFc7J0AcAFAwAGlJ0rsG";
+//FUNCTIONS
+//===============================================
+//MAIN PROCESSES
+//===============================================
+
+//1. Retrieve user inputs and convert to variables
+//2. Use those variabes to run an AJAX call to the New York Times
+//3. Break Down the NYT Object into usable fields
+//4. Dynamically generate html content 
+
+//5. Dealing with "edge cases" --bugs or situations that are not intuitive
